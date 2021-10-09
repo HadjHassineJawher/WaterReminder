19cd117870b9e3ceb869b9f86e492d468f286c05
AzizHannafi <hannafimedaziz@gmail.com>
2021-10-09 09:25:33 +0100

    My Last Updates

--- app/src/main/java/com/water/reminder/SigninFragment.java
+++ app/src/main/java/com/water/reminder/SigninFragment.java
@@ -1,11 +1,13 @@
 package com.water.reminder;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
@@ -17,6 +19,8 @@ public class SigninFragment extends Fragment {
         View v =inflater.inflate(R.layout.signin_fragment,container,false);
         TextView sign_up = v.findViewById(R.id.SignUp);
         Fragment sign_up_fragment= new SignupFragment();
+        EditText edt_email=v.findViewById(R.id.edt_email);
+        EditText edt_password= v.findViewById(R.id.edt_password);
 
         Button btnsignIn= v.findViewById(R.id.btn_login);
         sign_up.setOnClickListener(new View.OnClickListener() {
@@ -25,18 +29,48 @@ public class SigninFragment extends Fragment {
                 getFragmentManager().beginTransaction().replace(R.id.authentification_container,sign_up_fragment).commit();
 
             }
-
-
         });
         btnsignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent =new Intent(getContext(),MainActivity.class);
-                startActivity(intent);
+
+               checkEmail(edt_email);
+               checkPassword(edt_password);
+               if ((checkEmail(edt_email)==true)&&(checkPassword(edt_password))){
+                   Intent intent =new Intent(getContext(),MainActivity.class);
+                   startActivity(intent);
+               }
             }
         });
         return  v;
 
 
     }
+
+    public boolean checkEmail(EditText editText){
+        boolean res= true;
+        if(editText.getText().toString().trim().length()==0){
+            editText.setError("This field is empty!!!");
+            res=false;
+        }else if(!editText.getText().toString().contains("@")){
+            editText.setError("Invalid email !!!");
+            res=false;
+        }
+        return res;
+    }
+
+    public boolean checkPassword(EditText editText){
+        boolean res= true;
+        if(editText.getText().toString().trim().length()==0){
+            editText.setError("This field is empty!!!");
+            res=false;
+        }else if(editText.getText().toString().length()<6){
+            editText.setError("Password must contain at least seven characters !!!");
+            res=false;
+        }
+        return res;
+    }
+
 }
+
+
--- app/src/main/java/com/water/reminder/SignupFragment.java
+++ app/src/main/java/com/water/reminder/SignupFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 
@@ -13,7 +14,14 @@ public class SignupFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
           View v =inflater.inflate(R.layout.signup_fragment,container,false);
           Fragment sgininFragment= new SigninFragment();
-        Button btnBack= v.findViewById(R.id.back_to_signin);
+          Button btnBack= v.findViewById(R.id.back_to_signin);
+          Button btn_sgin_up=v.findViewById(R.id.btn_sgin_up);
+          EditText edt_email=v.findViewById(R.id.edt_email);
+          EditText edt_password=v.findViewById(R.id.edt_password);
+          EditText edt_confirme_password=v.findViewById(R.id.edt_confirme_password);
+          EditText edt_height=v.findViewById(R.id.edt_height);
+          EditText edt_weight=v.findViewById(R.id.edt_weight);
+
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,6 +30,65 @@ public class SignupFragment extends Fragment {
             }
         });
 
+        btn_sgin_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(
+                        (
+                                checkEmail(edt_email)&&
+                                checkPassword(edt_password)&&
+                                checkConfirmePassword(edt_confirme_password,edt_password)&&
+                                isEmpty(edt_height)&&
+                                isEmpty(edt_weight)
+                        )==true
+                ){
+                    getFragmentManager().beginTransaction().replace(R.id.authentification_container,sgininFragment).commit();
+                }
+
+            }
+        });
         return v;
+    }
+
+    public boolean checkEmail(EditText editText){
+        boolean res= true;
+        if(editText.getText().toString().trim().length()==0){
+            editText.setError("This field is empty!!!");
+            res=false;
+        }else if(!editText.getText().toString().contains("@")){
+            editText.setError("Invalid email !!!");
+            res=false;
+        }
+        return res;
+    }
+
+    public boolean checkPassword(EditText editText){
+        boolean res= true;
+        if(editText.getText().toString().trim().length()==0){
+            editText.setError("This field is empty!!!");
+            res=false;
+        }else if(editText.getText().toString().length()<6){
+            editText.setError("Password must contain at least seven characters !!!");
+            res=false;
+        }
+        return res;
+    }
+
+    public  boolean checkConfirmePassword(EditText editTextone,EditText editTexttow){
+        boolean res= true;
+        if(!(editTextone.getText().toString().equals(editTexttow.getText().toString()))){
+            editTextone.setError("Password dosent much !!!");
+            res= false;
+
+        }
+        return res;
+    }
+    public  boolean isEmpty(EditText editText){
+        boolean res= true;
+        if (editText.getText().length()==0){
+            editText.setError("This field is empty!!!");
+            res=false;
+        }
+        return res;
     }
 }
